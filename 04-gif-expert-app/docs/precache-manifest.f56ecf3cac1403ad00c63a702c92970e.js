eaa8457652e34bc16fdde6ab51b5827515532e51
Junior Ayala <elessar303@gmail.com>
2020-10-31 17:24:07 -0300

    gif app build as docs

---
+++ 04-gif-expert-app/docs/precache-manifest.f56ecf3cac1403ad00c63a702c92970e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5957f037f634a28e61c35dd58d5130c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21c0b747e4e127e97718",
+    "url": "/static/css/main.0c75d70b.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c0b747e4e127e97718",
+    "url": "/static/js/main.653b2502.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
